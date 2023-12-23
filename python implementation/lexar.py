82f07b35c517b9a22885fdd569b8382b59db15bc
Ashley Czumak <ashleyczumak@Ashleys-MacBook-Pro-2.local>
2023-12-23 10:46:03 -0500

    very easy stuff

---
+++ python implementation/lexar.py
@@ -0,0 +1,41 @@
+
+class Lexer:
+    def __init__(self, input_data):
+        self.input = input_data
+        self.position = 0
+        self.tokens = []
+
+    def tokenize(self):
+        while self.position < len(self.input):
+            char = self.input[self.position]
+            if char in [' ', '\t', '\n', '\r']:
+                self.position += 1
+            elif char == '+':
+                self.tokens.append(('PLUS', char))
+                self.position += 1
+            elif char == '(':
+                self.tokens.append(('LPAREN', char))
+                self.position += 1
+            elif char == ')':
+                self.tokens.append(('RPAREN', char))
+                self.position += 1
+            elif char == '*':
+                self.tokens.append(('MULT', char))
+            elif char.isdigit():
+                self.tokens.append(('INTEGER', self._read_integer()))
+            else:
+                raise Exception(f"Illegal character: {char}")
+
+        return self.tokens
+
+    def read_integer(self):
+        start_position = self.position
+        while self.position < len(self.input) and self.input[self.position].isdigit():
+            self.position += 1
+        return self.input[start_position:self.position]
+
+
+data = "3 + (4 + 5) * 9 + 10"
+lexer = Lexer(data)
+tokens = lexer.tokenize()
+print(tokens)
