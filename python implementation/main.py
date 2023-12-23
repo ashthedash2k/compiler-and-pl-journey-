9233b50a607207d8ef7d6dc4512e571caa4ce1ba
Ashley Czumak <aczumak@andrew.cmu.edu>
2023-12-23 16:57:55 -0500

    main kinda dead lol, ill fix later, testing out parser in parser.py rn

--- python implementation/main.py
+++ python implementation/main.py
@@ -1,12 +1,17 @@
 '''
 Run everything in here. Read in a file of python code. 
 '''
+from lexer import Lexer
+from parser import Parser  
 
-def main():
-    file_path = '/Users/ashthedash/compiler_journey/python implementation/code.txt'
+def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
-
-if __name__ == '__main__':
+def main():
+    file_path = '/Users/ashthedash/compiler_journey/python implementation/code.txt'
+    code = read_file(file_path)
+    
+if __name__ == "__main__":
     main()
+5
