95585879dbe9b3c96ab1c2bc7ec913707cd8ea1e
Ashley Czumak <aczumak@andrew.cmu.edu>
2023-12-23 19:47:42 -0500

    crying

--- python implementation/parser.py
+++ python implementation/parser.py
@@ -9,29 +9,45 @@ class Parser:
     def next_token(self):
         self.curr_token = next(self.tokens, None)
     
-    def addition(self):
-        result = 0 
+    def operands(self):
+        if self.curr_token and self.curr_token[0] in ['INTEGER', 'FLOAT']:
+            result = float(self.curr_token[1]) if self.curr_token[0] == 'FLOAT' else int(self.curr_token[1])
+            self.next_token()
+        else:
+            raise SyntaxError("Expected a number at the beginning of expression")
+
         while self.curr_token:
-            if self.curr_token[0] in ['INTEGER', 'FLOAT']:
-                if self.curr_token[0] == 'FLOAT':
-                    result += float(self.curr_token[1])
-                elif self.curr_token[0] == 'INTEGER':
-                    result += int(self.curr_token[1])
+            if self.curr_token[0] == 'PLUS':
+                self.next_token()
+                if self.curr_token[0] in ['INTEGER', 'FLOAT']:
+                    result += float(self.curr_token[1]) if self.curr_token[0] == 'FLOAT' else int(self.curr_token[1])
                 else:
-                    print(f'{self.curr_token[0]} was parsed\n')
-                    ValueError('Cannot parse this object')
+                    raise SyntaxError("Expected a number after '+'")
+            elif self.curr_token[0] == 'MULT':
                 self.next_token()
-            elif self.curr_token[0] == 'PLUS':
+                if self.curr_token[0] in ['INTEGER', 'FLOAT']:
+                    result *= float(self.curr_token[1]) if self.curr_token[0] == 'FLOAT' else int(self.curr_token[1])
+                else:
+                    raise SyntaxError("Expected a number after '*'")
+            elif self.curr_token[0] == 'MINUS':
                 self.next_token()
+                if self.curr_token[0] in ['INTEGER', 'FLOAT']:
+                    result -= float(self.curr_token[1]) if self.curr_token[0] == 'FLOAT' else int(self.curr_token[1])
+                else:
+                    raise SyntaxError("Expected a number after '-'")
+
             else:
-                raise SyntaxError("Unexpected token!")
+                raise SyntaxError("Unexpected token")
+
+            self.next_token()
 
         return result
 
 
-data = "1 + 2.5 + 3"
+
+data = "1 + 2 * 3 + 4 - 10"
 lexer = Lexer(data)
 tokens = lexer.tokenize()
 parser = Parser(tokens)
-result = parser.addition()
+result = parser.operands()
 print("Result:", result)  
